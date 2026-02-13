8a88040eab46534b78ef8bac20a99c5552688919
Himanshu Prakash <prakashhimanshu88@gmail.com>
2026-02-14 01:53:12 +0530

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directories
+mkdir -p scr
+mkdir -p research
+
+#creating files for the project
+touch scr/__init__.py
+touch scr/helper.py
+touch scr/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Project structure created successfully!"
